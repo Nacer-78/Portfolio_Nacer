f5f2a3be4b6933002f93a275d187a98acc4a89f0
Nacer-78 <nacer.bouamama.78@gmail.com@gmail.com>
2025-09-25 15:52:43 +0200

    url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio_nacer/",
+  base: "/Portfolio_Nacer/",
   plugins: [react(), tailwindcss()],
 })
